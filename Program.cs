de5ee3d588a1e002a940d9a70f0c6006352e18cf
Paygammy <email.sjb0@gmail.com>
2022-07-09 17:42:41 -0700

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using MoonSharp.Interpreter;
+
+Script script = new Script();
+
+while (true)
+{
+    string? source = Console.In.ReadLine();
+    if (source == null || string.IsNullOrEmpty(source)) continue;
+    // Custom Environment Here
+
+    // Execute Script in Console
+    try { script.DoString(source); } catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+}
